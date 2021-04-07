39ec94c21a681b7585f560877a0d63ef2bc59a79
Cloud <alice123357@gmail.com>
2021-04-07 15:19:41 +0800

    12

--- BuyFood_Template/Controllers/cSalesAnalysisController.cs
+++ BuyFood_Template/Controllers/cSalesAnalysisController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace adminCode.Controllers
+namespace BuyFood_Template.Controllers
 {
     public class cSalesAnalysisController : Controller
     {
